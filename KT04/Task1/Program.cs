b154e089705a18aef18a08dd27075e48fce88a05
Artak <artak.oganisyan07@gmail.com>
2025-12-10 19:06:10 +0300

    Kt04

---
+++ KT04/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+
+class LogWriter : IDisposable
+{   
+    private readonly ILog _log;
+    private readonly ILogWriter _writer;
+    
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    public LogWriter(ILog log,  ILogWriter writer)
+    {
+        _log = log;
+        _writer = writer;
+    }
+    //хз крч я че то затупил, похоже на бред(((
+}
